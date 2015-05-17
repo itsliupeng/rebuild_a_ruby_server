c91f650d096c01fb3a400756e893fbfc0bfafc53
liupeng <itsliupeng@gmail.com>
2015-05-17 12:32:59 +0800

    little error

--- tube.rb
+++ tube.rb
@@ -37,6 +37,65 @@ class Tube
     end
   end
 
+  class Connection
+    def initialize(socket, app)
+      @socket = socket
+      @app = app
+      @parser = Http::Parser.new(self)
+    end
+
+    def process
+      until @socket.closed? || @socket.eof?
+        data = @socket.readpartial(1024)
+        @parser << data
+      end
+    end
+
+    def on_message_complete
+      puts "#{@parser.http_method} #{@parser.request_path}"
+      puts "  " + @parser.headers.inspect
+      puts
+
+      env = {}
+      @parser.headers.each_pair do |name, value|
+        # User-Agent => HTTP_USER_AGENT
+        name = "HTTP_" + name.upcase.tr("-", "_")
+        env[name] = value
+      end
+      env["PATH_INFO"] = @parser.request_path
+      env["REQUEST_METHOD"] = @parser.http_method
+      env["rack.input"] = StringIO.new
+
+      send_response env
+    end
+
+    REASONS = {
+      200 => "OK",
+      404 => "Not found"
+    }
+
+    def send_response(env)
+      status, headers, body = @app.call(env)
+      reason = REASONS[status]
+
+      @socket.write "HTTP/1.1 #{status} #{reason}\r\n"
+      headers.each_pair do |name, value|
+        @socket.write "#{name}: #{value}\r\n"
+      end
+      @socket.write "\r\n"
+      body.each do |chunk|
+        @socket.write chunk
+      end
+      body.close if body.respond_to? :close
+
+      close
+    end
+
+    def close
+      @socket.close
+    end
+  end
+
   class EMConnection < EM::Connection
     attr_accessor :app
 
